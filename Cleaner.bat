e6c93ef8948e8b628adfd2557456db89f81451e9
Prashant Thakur <149857084+ShortageOfName@users.noreply.github.com>
2025-09-20 13:42:29 +0530

    Update Cleaner.bat
    Added support for LocalAppData and Windows Temp cleanup

--- Cleaner.bat
+++ Cleaner.bat
@@ -1,11 +1,22 @@
 @echo off
 echo =========================================
-echo   🧹 Basic Temp Cleaner
+echo   🧹 Temp Cleaner - Extended
 echo =========================================
 
+:: Clean %TEMP%
 del /q /f /s "%TEMP%\*" 2>nul
 rd /s /q "%TEMP%" 2>nul
 md "%TEMP%"
+
+:: Clean %localappdata%\Temp
+del /q /f /s "%localappdata%\Temp\*" 2>nul
+rd /s /q "%localappdata%\Temp" 2>nul
+md "%localappdata%\Temp"
+
+:: Clean C:\Windows\Temp
+del /q /f /s "C:\Windows\Temp\*" 2>nul
+rd /s /q "C:\Windows\Temp" 2>nul
+md "C:\Windows\Temp"
 
 echo.
 echo Cleanup Finished!
